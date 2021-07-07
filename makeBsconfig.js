e7c981c84c5d20734e51c4ee64c2a6b957fe2d4b
Alain Armand <idkjs@users.noreply.github.com>
2021-07-07 15:42:14 +0200

    inish

---
+++ makeBsconfig.js
@@ -0,0 +1,57 @@
+const fs = require("fs");
+
+// if (process.argv.length === 2) {
+//   console.error("Expected one argument to atdgen binary");
+//   process.exit(1);
+// }
+
+// const atdgenPath = process.argv[2];
+
+const bsconfigJson = JSON.stringify(
+  {
+    name: "bs-atdgen-generator-integration-test",
+    generators: [
+      {
+        name: "atd_t",
+        command: `npx atdgen -t $in`,
+      },
+      {
+        name: "atd_bs",
+        command: `npx atdgen -bs $in`,
+      },
+    ],
+    sources: [
+      "src",
+      {
+        dir: "src/atd",
+        generators: [
+          {
+            name: "atd_t",
+            edge: ["Foo_t.ml", "Foo_t.mli", ":", "Foo.atd"],
+          },
+          {
+            name: "atd_bs",
+            edge: ["Foo_bs.ml", "Foo_bs.mli", ":", "Foo.atd"],
+          },
+        ],
+      },
+    ],
+    "package-specs": {
+      module: "commonjs",
+      "in-source": true,
+    },
+    suffix: ".bs.js",
+    "bs-dependencies": ["@ahrefs/bs-atdgen-codec-runtime"],
+    warnings: {
+      error: "+101",
+    },
+    "generate-merlin": true,
+    namespace: true
+  },
+  null,
+  2
+);
+
+fs.writeFileSync("bsconfig.json", bsconfigJson, {
+  encoding: "utf8",
+});
